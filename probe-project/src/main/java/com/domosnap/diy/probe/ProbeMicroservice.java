ec234cb4f16e6e572e5c3a200a82708ce8dec924
DE GIULI Arnaud <arnaud.degiuli@free.fr>
2020-01-04 21:49:30 +0100

    Add docker file.

--- probe-project/src/main/java/com/domosnap/diy/probe/ProbeMicroservice.java
+++ probe-project/src/main/java/com/domosnap/diy/probe/ProbeMicroservice.java
@@ -32,19 +32,32 @@ import com.domosnap.engine.event.EventFactory;
 import com.domosnap.engine.event.EventToConsoleConsumer;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.ext.healthchecks.HealthCheckHandler;
+import io.vertx.ext.healthchecks.HealthChecks;
+import io.vertx.ext.healthchecks.Status;
+import io.vertx.ext.web.Router;
 
 public class ProbeMicroservice extends AbstractVerticle {
 	
 	@Override
 	public void start() {
 
-		Log.getConfig().put("all", "false");
+		Log.getConfig().put("all", "true");
 		
 		EventFactory.addConsumer(new EventToConsoleConsumer());
+		
+//		MqttClientOptions opt = new MqttClientOptions();
+//		opt.setMaxInflightQueue(100);
+//		EventFactory.addConsumer(new EventToMqttConsumer("env-5291014.hidora.com", 11112, opt, getVertx(), null, true));
 
 		OpenWebNetControllerAdapter own = new OpenWebNetControllerAdapter("scs://12345@192.168.1.35:20000");
-		own.connect();
-		own.scan(new ScanListerImpl("scs"));
+		
+		vertx.executeBlocking(future -> {
+			own.connect();
+			own.scan(new ScanListerImpl("scs"));
+		}, null);
+		
 		
 		OneWireControllerAdapter owa = new OneWireControllerAdapter();
 		owa.connect();
@@ -53,6 +66,22 @@ public class ProbeMicroservice extends AbstractVerticle {
 		I2CControllerAdapter i2c = new I2CControllerAdapter();
 		i2c.connect();
 		i2c.scan(new ScanListerImpl("I2C"));
+		
+		
+		// Create healthcheck
+		HealthChecks hc = HealthChecks.create(vertx);
+
+		// Register with a timeout. The check fails if it does not complete in time.
+		// The timeout is given in ms.
+		hc.register("ownCheckHealth", 200, future -> {
+			future.complete(Status.OK());
+		});
+		Router router = Router.router(vertx);
+		// Register the health check handler
+		router.get("/health*").handler(HealthCheckHandler.createWithHealthChecks(hc));
+		vertx.createHttpServer().requestHandler(router).listen(8080, ar -> {
+			System.out.println("Health started on port " + ar.result().actualPort());
+		});
 		
 		System.out.println("Probe Service Started");
 	}
@@ -64,7 +93,8 @@ public class ProbeMicroservice extends AbstractVerticle {
 	}
 
 	public static void main(String[] args) {
-		io.vertx.core.Vertx vertx = io.vertx.core.Vertx.vertx();
+//		io.vertx.core.Vertx vertx = io.vertx.core.Vertx.vertx();
+		Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(ProbeMicroservice.class.getName());
 	}
 }
